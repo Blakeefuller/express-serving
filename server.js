c4fe0b183fd54aec5702a5eee093b3c7f1ffb43a
Blakeefuller <55156679+Blakeefuller@users.noreply.github.com>
2021-11-16 15:49:04 -0800

    more class work

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express')
+
+var app = express()
+
+app.use(function (req, res, next) {
+    console.log("== Request received")
+    console.log("  -- req.url:", req.url)
+    console.log("  -- req.method:", req.method)
+    next()
+    //res.send()  
+})
+
+app.use(function (req, res, next) {
+    console.log("  -- Second middleware")
+    next()
+    //res.send()  
+})
+
+app.use(function (req, res, next) {
+    console.log("  -- Third middleware")
+    next()
+    //res.send()  
+})
+
+app.get('/about', function (req, res, next) {
+    console.log("  -- about page requested")
+    res.status(200).send("<html><body><h1> About page </h1></body></html>")
+})
+
+app.get('/', function (req, res, next) {
+    console.log("  -- home page requested")
+    res.status(200).sendFile(__dirname + '/public/index.html')
+})
+
+app.get('*', function (req, res, next) {
+    console.log("  -- 404!")
+    res.status(404).sendFile(__dirname + '/public/404.html')
+})
+
+//app.post()
+//app.patch()
+//app.put()
+//app.delete()
+
+app.listen(8000, function () {
+    console.log("== Server is listening on port 8000")
+})
